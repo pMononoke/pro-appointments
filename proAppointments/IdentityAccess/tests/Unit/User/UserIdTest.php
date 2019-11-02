127da365e234f20a07305c10cfae053d42d8d478
pMononoke <francescabonadonna@gmail.com>
2019-11-02 18:35:50 +0100

    IdentityAccess: add userId.

---
+++ proAppointments/IdentityAccess/tests/Unit/User/UserIdTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ProAppointments\IdentityAccess\Tests\Unit\User;
+
+use PHPUnit\Framework\TestCase;
+use ProAppointments\IdentityAccess\Domain\User\UserId;
+
+class UserIdTest extends TestCase
+{
+    private const UUID = '100ade38-ad18-4cf4-9ac5-de2b00c4abb7';
+
+    /** @test */
+    public function can_autogenerate_a_userId_object(): void
+    {
+        self::assertInstanceOf(UserId::class, UserId::generate());
+    }
+
+    /** @test */
+    public function can_be_created_from_string(): void
+    {
+        $userId = UserId::fromString(self::UUID);
+
+        self::assertEquals(self::UUID, $userId->toString());
+    }
+
+    /** @test */
+    public function can_be_compared(): void
+    {
+        $first = UserId::fromString(self::UUID);
+        $second = UserId::generate();
+        $copyOfFirst = UserId::fromString(self::UUID);
+
+        self::assertFalse($first->equals($second));
+        self::assertTrue($first->equals($copyOfFirst));
+        self::assertFalse($second->equals($copyOfFirst));
+    }
+}
