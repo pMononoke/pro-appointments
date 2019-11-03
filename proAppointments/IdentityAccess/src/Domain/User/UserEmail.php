25958db1b496d94ddc7e2fbca035f5f57e4685ff
pMononoke <francescabonadonna@gmail.com>
2019-11-03 10:50:37 +0100

    IdentityAccess: add user email.

---
+++ proAppointments/IdentityAccess/src/Domain/User/UserEmail.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ProAppointments\IdentityAccess\Domain\User;
+
+final class UserEmail
+{
+    private $value;
+
+    public function __construct(string $value)
+    {
+        if (strlen($value) < 5) {
+            throw new \InvalidArgumentException('User email is too short.');
+        }
+
+        $this->value = $value;
+    }
+
+    public function value(): string
+    {
+        return $this->value;
+    }
+
+    public static function fromString(string $userEmail): UserEmail
+    {
+        return new self($userEmail);
+    }
+
+    public function toString(): string
+    {
+        return $this->value;
+    }
+
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+
+    public function equals(UserEmail $userEmail): bool
+    {
+        return $this->value === $userEmail->value;
+    }
+}
---
+++ proAppointments/IdentityAccess/tests/Unit/User/UserEmailTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ProAppointments\IdentityAccess\Tests\Unit\User;
+
+use PHPUnit\Framework\TestCase;
+use ProAppointments\IdentityAccess\Domain\User\UserEmail;
+
+class UserEmailTest extends TestCase
+{
+    private const EMAIL = 'irrelevant@example.com';
+
+    /** @test */
+    public function can_be_created_from_string(): void
+    {
+        $firstName = UserEmail::fromString(self::EMAIL);
+
+        self::assertInstanceOf(UserEmail::class, $firstName);
+        self::assertEquals(self::EMAIL, $firstName->toString());
+    }
+
+    /** @test */
+    public function can_be_compared(): void
+    {
+        $first = UserEmail::fromString(self::EMAIL);
+        $second = UserEmail::fromString('other-irrelevant@example.com');
+        $copyOfFirst = UserEmail::fromString(self::EMAIL);
+
+        self::assertFalse($first->equals($second));
+        self::assertTrue($first->equals($copyOfFirst));
+        self::assertFalse($second->equals($copyOfFirst));
+    }
+
+    /**
+     * @test
+     * @expectedException \InvalidArgumentException
+     */
+    public function a_short_email_throw_exception(): void
+    {
+        UserEmail::fromString('a');
+    }
+}
