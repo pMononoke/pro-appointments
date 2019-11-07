ab9aa89c7544a0bc2ff2b7d169cd55280fdc64b1
pMononoke <francescabonadonna@gmail.com>
2019-11-07 13:35:37 +0100

    Modify ApplicationService interface.

--- lib/compost/src/ApplicationService/ApplicationService.php
+++ lib/compost/src/ApplicationService/ApplicationService.php
@@ -4,7 +4,15 @@ declare(strict_types=1);
 
 namespace CompostDDD\ApplicationService;
 
+/**
+ * Interface ApplicationService.
+ */
 interface ApplicationService
 {
-    public function execute($request);
+    /**
+     * @param object $request
+     *
+     * @return mixed
+     */
+    public function execute(object $request);
 }
