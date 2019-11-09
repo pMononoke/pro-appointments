d060c1d69474b59ae16f6a13c0cc78b0408ee302
pMononoke <francescabonadonna@gmail.com>
2019-11-09 16:39:22 +0100

    IdentityAccess: add UserWasRegistered event.

---
+++ proAppointments/IdentityAccess/src/Domain/User/Event/UserWasRegistered.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace ProAppointments\IdentityAccess\Domain\User\Event;
+
+use ProAppointments\IdentityAccess\Domain\User\UserEmail;
+use ProAppointments\IdentityAccess\Domain\User\UserId;
+use ProAppointments\IdentityAccess\Domain\User\UserPassword;
+
+class UserWasRegistered
+{
+    private const EVENT_NAME = '';
+
+    private $userId;
+
+    private $email;
+
+    private $password;
+
+    /** @var \DateTimeImmutable */
+    private $occurredOn;
+
+    public function __construct(UserId $userId, UserEmail $email, ?UserPassword $password = null)
+    {
+        $this->userId = $userId;
+        $this->email = $email;
+        $this->password = $password;
+        $this->occurredOn = new \DateTimeImmutable();
+    }
+
+    /**
+     * @return UserId
+     */
+    public function userId(): UserId
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @return UserEmail
+     */
+    public function email(): UserEmail
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return UserPassword
+     */
+    public function password(): ?UserPassword
+    {
+        return $this->password;
+    }
+
+    /**
+     * @return \DateTimeImmutable
+     */
+    public function occurredOn(): \DateTimeImmutable
+    {
+        return $this->occurredOn;
+    }
+
+    public function toPayload(): array
+    {
+        return [
+            'userId' => $this->userId->toString(),
+            'email' => $this->email->toString(),
+        ];
+    }
+}
