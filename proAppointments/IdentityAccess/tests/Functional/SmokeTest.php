a5864ce2073ac1313e6201c8f627f158a69c02a6
pMononoke <francescabonadonna@gmail.com>
2021-01-13 12:56:47 +0100

    Improve smoke tests.

--- proAppointments/IdentityAccess/tests/Functional/SmokeTest.php
+++ proAppointments/IdentityAccess/tests/Functional/SmokeTest.php
@@ -5,42 +5,32 @@ declare(strict_types=1);
 namespace ProAppointments\IdentityAccess\Tests\Functional;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\BrowserKit\Client;
 
 /**
  * @group functional
  */
 class SmokeTest extends WebTestCase
 {
-    /** @var Client */
-    private static $client;
-
-    protected function setUp(): void
-    {
-        //self::$client = static::createClient();
-        //self::$client->followRedirect(true);
-        //self::$container = self::$client->getContainer();
-    }
-
     /**
      * @test
      * @dataProvider identityRouteProvider
      */
     public function identityControllers(string $path): void
     {
-        $webserver = 'http://199.0.0.1';
-        $client = static::createClient([], ['HTTP_HOST' => $webserver]);
+        $client = static::createClient();
+
         $client->request('GET', $path);
-        \var_dump($client->getResponse()->getContent());
         $this->assertTrue($client->getResponse()->isSuccessful());
-
-
     }
 
     public function identityRouteProvider(): \Generator
     {
         yield ['/'];
-        //yield ['/identity'];
-        //yield ['/identity/foo'];
+        yield ['/im_a_vue_rute'];
+        yield ['/administration'];
+        yield ['/administration/im_a_vue_rute'];
+        yield ['/identity'];
+        yield ['/identity/im_a_vue_rute'];
+        //yield ['/appointment/new'];
     }
 }
