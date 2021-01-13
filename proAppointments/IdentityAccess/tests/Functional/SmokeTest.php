4d6a92b2ff7fc216bcaa406b42ed8d08029ff49a
pMononoke <francescabonadonna@gmail.com>
2021-01-13 11:48:12 +0100

    Fix smoke test.

--- proAppointments/IdentityAccess/tests/Functional/SmokeTest.php
+++ proAppointments/IdentityAccess/tests/Functional/SmokeTest.php
@@ -5,22 +5,36 @@ declare(strict_types=1);
 namespace ProAppointments\IdentityAccess\Tests\Functional;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Client;
 
 /**
  * @group functional
  */
 class SmokeTest extends WebTestCase
 {
+    /** @var Client */
+    private static $client;
+
+    protected function setUp(): void
+    {
+        //self::$client = static::createClient();
+        //self::$client->followRedirect(true);
+        //self::$container = self::$client->getContainer();
+    }
+
     /**
      * @test
      * @dataProvider identityRouteProvider
      */
     public function identityControllers(string $path): void
     {
-        $webserver = 'http://127.0.0.1';
+        $webserver = 'http://199.0.0.1';
         $client = static::createClient([], ['HTTP_HOST' => $webserver]);
         $client->request('GET', $path);
+        \var_dump($client->getResponse()->getContent());
         $this->assertTrue($client->getResponse()->isSuccessful());
+
+
     }
 
     public function identityRouteProvider(): \Generator
