740e94127e999194755d33ccb86b2d9b6cf98e18
pMononoke <francescabonadonna@gmail.com>
2021-01-29 14:36:38 +0100

    Added user factory girl.

--- config/bundles.php
+++ config/bundles.php
@@ -13,5 +13,4 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
